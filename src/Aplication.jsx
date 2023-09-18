16d38db1609c253d7a88e134d2cafc3511e8e2df
mayo <jruso2008@gmail.com>
2023-09-18 14:24:44 -0300

    Modificacion de Interfaz Realizada

--- src/Aplication.jsx
+++ src/Aplication.jsx
@@ -1,24 +1,14 @@
 import MailBox from "./components/MailBox";
-<<<<<<< HEAD
 import Editing from "./components/Editing";
-function Aplication() {
-  return (
-    <>
-      <MailBox></MailBox>
-      <Editing></Editing>
-=======
-import Editing from "./components/Editing"
-import Header from "./components/Header";
 function Aplication() {
   return (
     <>
     <Header></Header>
     <MailBox></MailBox>
     <Editing></Editing>
->>>>>>> c9d81587c574f03e71e5f3716cb900e6e24a9139
     </>
   );
-}
+  }
 //mail box es para las targetitas
 
 //editing es para el pop up 
