2b32ecc4e6735ca6f31355d81b0dafc6e1eb5b30
Ry0s.dev <117836353+Ryos7x@users.noreply.github.com>
2023-09-18 15:40:27 -0300

    modificación

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 function Header() {
   return (
     <header>
-        <ul className="flex flex-row bg-slate-700 p-5 align-middle justify-between text-4xl">
-            <li>Julian Mayola</li>
-            <li>Elias Tello</li>
-        </ul>
-        </header>
-  )
+      <ul className="flex flex-row bg-gray-500 p-5 align-middle justify-between text-4xl">
+        <li>Julian Mayola</li>
+        <li>Elias Tello</li>
+      </ul>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
--- src/components/ShowComments.jsx
+++ src/components/ShowComments.jsx
@@ -18,11 +18,11 @@ function ShowComments() {
     <CommentsContextProvider>
       {Comments.map((Comments) => (
         <div key={Comments.id} onClick={() => EditComment(Comments.id)}>
-          <div className=" m-auto bg-[#8F0F0F] p-4 m-5 hover:border-[#0D113C] border-[#1B285E] border-4 rounded-3xl box-border text-slate-100 w-96 justify-center align-middle">
+          <div className=" m-auto  bg-[#b6a4a4] p-4  hover:border-[#78ff67] border-[#03050a] border-4 rounded-3xl box-border text-slate-100 w-96 justify-center align-middle">
             <img
               src={Comments.image}
               alt=""
-              className="m-auto justify-center align-middle"
+              className="m-auto justify-center align-middle hover:drop-shadow-sm"
             />
           </div>
         </div>
