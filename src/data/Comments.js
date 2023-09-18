73c61d03086d667a05c16d50f5ae273dde4a4acd
mayo <jruso2008@gmail.com>
2023-09-18 14:10:23 -0300

    asd

--- src/data/Comments.js
+++ src/data/Comments.js
@@ -1,19 +1,23 @@
 export const Comments = [
     {
         name: 'Grifindor',
+        image: '',
         message: 'jdaisjdiajsd',
         id: 0,
     },{
         name: 'gen',
         message: 'asdasd',
+        image: '',
         id: 1,
     },
     {
         name: 'Grifindor',
         message: 'jdaisjdiajsd',
+        image: '',
         id: 0,
     },{
         name: 'gen',
+        image: '',
         message: 'asdasd',
         id: 1,
     }
